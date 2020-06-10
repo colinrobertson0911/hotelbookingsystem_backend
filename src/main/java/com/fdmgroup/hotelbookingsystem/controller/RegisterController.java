c338e00142f1be1ceb9f314efabddf31eafda74e
simon <simoncoffey9@gmail.com>
2020-06-10 09:56:40 +0100

    changed registration form to return jwt

--- src/main/java/com/fdmgroup/hotelbookingsystem/controller/RegisterController.java
+++ src/main/java/com/fdmgroup/hotelbookingsystem/controller/RegisterController.java
@@ -1,11 +1,18 @@
 package com.fdmgroup.hotelbookingsystem.controller;
 
+import com.fdmgroup.hotelbookingsystem.model.AuthenticationResponse;
 import com.fdmgroup.hotelbookingsystem.model.User;
+import com.fdmgroup.hotelbookingsystem.services.JWTservice;
+import com.fdmgroup.hotelbookingsystem.services.MyUserDetailsService;
 import com.fdmgroup.hotelbookingsystem.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,17 +22,38 @@ public class RegisterController {
 
     @Autowired
     UserService userService;
+    
+    @Autowired
+    private AuthenticationManager authManager;
+    
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+    
+    @Autowired
+    private JWTservice jwtService;
 
     @PostMapping("/RegisterUserSubmit")
-    public ResponseEntity<HttpStatus> registerUserSubmit(@RequestBody User user){
+    public ResponseEntity<?> registerUserSubmit(@RequestBody User user) throws Exception{
         try {
             userService.save(user);
         }catch(DataIntegrityViolationException e) {
             return new ResponseEntity<HttpStatus> (HttpStatus.CONFLICT);
         }
-        return ResponseEntity.ok(HttpStatus.CREATED);
+        
+        
+        try {
+			authManager.authenticate(
+					new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("incorrect Username or password", e);
+		}
+	
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(user.getUsername());
+		final String jwt = jwtService.generateToken(userDetails);
+		
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 
     }
-
 }
 
